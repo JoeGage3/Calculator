aaa104086baccc7d656445cb5c82669798620546
Joseph Gage <joegage@Unknown-3c-15-c2-d2-f2-90.home>
2018-03-31 15:29:44 +0100

    Added more operations using a switch statment

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
-console.log('Please enter the first number to be multiplied.');
-const num1 = readline.prompt();
-console.log('Please enter the second number to be multiplied.');
-const num2 = readline.prompt();
-const prod = (+num1)*(+num2);
-console.log('The answer is ' + prod + '.');
+console.log('==========================');
+
+console.log('\nPlease enter the operator:');
+const operator = readline.prompt();
+
+console.log('\nPlease enter the first number:');
+const input1 = readline.prompt();
+const num1 = +input1;
+
+console.log('\nPlease enter the second number:');
+const input2 = readline.prompt();
+const num2 = +input2;
+
+switch(operator) {
+    case '+': {
+            const answer = num1 + num2;
+            console.log('\nThe answer is: ' + answer + '.');
+        }
+        break;
+    case '-': {
+            const answer = num1 - num2;
+            console.log('\nThe answer is: ' + answer + '.');
+        }
+        break;
+    case '*': {
+            const answer = num1 * num2;
+            console.log('\nThe answer is: ' + answer + '.');
+        }
+        break;
+    case '/': {
+            const answer = num1 / num2;
+            console.log('\nThe answer is: ' + answer + '.');
+        }
+        break;
+    default:
+        console.log('\nSorry, ' + operator + ' is not a valid operator. This calculator only supports the operators +, -, *, and /.');
+        break;
+}
